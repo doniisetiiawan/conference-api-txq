1b118059ea953e05119860fbcb3ee12a70464837
Doni Setiawan <donisetiawan.050@gmail.com>
2020-03-27 20:54:18 +0700

    Adding the Passport middleware

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import flash from 'connect-flash';
+import passport from 'passport';
+import session from 'express-session';
+import MongoStoretmo from 'connect-mongo';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
 
+import routes from './server/routes/index';
 import speakers from './server/routes/speakers';
 import config from './server/config/config';
+import passporteta from './server/config/passport';
+
+const MongoStore = MongoStoretmo(session);
 
 const app = express();
 
+app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 mongoose.connect(config.url, {
   useNewUrlParser: true,
@@ -20,14 +32,43 @@ mongoose.connection.on('error', () => {
   );
 });
 
-const router = express.Router();
+passporteta(passport);
 
-router.use((req, res, next) => {
-  console.log('An action was performed by the server.');
-  next();
+app.use(
+  session({
+    secret: 'multifunctional',
+    saveUninitialized: true,
+    resave: true,
+    store: new MongoStore({
+      url: config.url,
+      collection: 'sessions',
+    }),
+  }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use('/', routes);
+app.use('/api/speakers', speakers);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.use('/api/speakers', speakers);
+if (app.get('env') === 'development') {
+  // eslint-disable-next-line no-unused-vars
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err,
+    });
+  });
+}
 
 app.set('port', process.env.PORT || 3000);
 
